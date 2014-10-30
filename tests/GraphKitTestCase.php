f1d82be88f5680d577618382e0b7146e19350b25
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-10-30 22:14:35 +0100

    removed full class name in method

--- tests/GraphKitTestCase.php
+++ tests/GraphKitTestCase.php
@@ -9,7 +9,7 @@ class GraphKitTestCase extends \PHPUnit_Framework_TestCase
 {
     protected function buildRealClient()
     {
-        $neo4jClient = new \Everyman\Neo4j\Client('localhost', 7474);
+        $neo4jClient = new Client('localhost', 7474);
 
         return $neo4jClient;
     }
