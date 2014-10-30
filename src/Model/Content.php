2b011c39105779e174c6da494da0668c53f8ba15
jeremykendall <jeremy@jeremykendall.net>
2014-10-30 10:15:24 -0500

    Adds Content::toArray(), updates properties

--- src/Model/Content.php
+++ src/Model/Content.php
@@ -11,6 +11,23 @@ class Content
     public $tagstr;
     public $timestamp;
     public $uuid;
+    public $contentId;
     public $userNameForPost;
-    public $owner;
+    public $owner = false;
+
+    public function toArray()
+    {
+        return array(
+            'id' => $this->id,
+            'title' => $this->title,
+            'url' => $this->url,
+            'tagstr' => $this->tagstr,
+            'timestamp' => $this->timestamp,
+            'uuid' => $this->uuid,
+            'contentId' => $this->uuid,
+            'userNameForPost' => $this->userNameForPost,
+            'owner' => $this->owner,
+            'node' => $this->node,
+        );
+    }
 }
