7fbbd2b2bdf1ba1d960408377e2c74e36e79672d
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-10-30 23:18:19 +0100

    more convenience user service test

--- tests/GraphKitTestCase.php
+++ tests/GraphKitTestCase.php
@@ -3,6 +3,7 @@
 namespace GraphStory\GraphKit\Tests;
 
 use Everyman\Neo4j\Client;
+use Everyman\Neo4j\Cypher\Query;
 use GraphStory\GraphKit\Neo4jClient;
 
 class GraphKitTestCase extends \PHPUnit_Framework_TestCase
@@ -15,5 +16,19 @@ class GraphKitTestCase extends \PHPUnit_Framework_TestCase
         $appClient = Neo4jClient::setClient($neo4jClient);
 
         return $appClient;
+    }
+
+    protected function sendQuery($q)
+    {
+        $queryString = $q;
+        $query = new Query(Neo4jClient::client(), $queryString);
+        $result = $query->getResultSet();
+
+        return $result;
+    }
+
+    protected function clearDB()
+    {
+        return $this->sendQuery('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE r,n');
     }
 }
--- tests/Service/UserServiceTest.php
+++ tests/Service/UserServiceTest.php
@@ -8,21 +8,52 @@ use GraphStory\GraphKit\Model\User,
 
 class UserServiceTest extends GraphKitTestCase
 {
-    public function testCreateUser()
+
+    private $standardUser;
+
+    private $standardUserArray;
+
+    public function __construct()
     {
-        $this->buildRealClient();
         $user = new User();
-        $user->id = mt_rand(1, 100);
-        $user->username = 'serialgrapher'.$user->id;
+        $user->id = 12345678;
+        $user->username = 'serialgrapher';
         $user->firstname = 'Roger';
         $user->lastname = 'McCarthy';
+        $this->standardUser = $user;
+        $this->standardUserArray = $user->toArray();
+    }
+
+    public function testCreateAndGetUser()
+    {
+        $this->buildRealClient();
+        $this->clearDB();
+        $user = $this->standardUser;
         $userService = new UserService();
         $userService->save($user);
-
         $fetchedUser = $userService->getByUsername($user->username);
+
         $this->assertEquals($fetchedUser->username, $user->username);
         $this->assertEquals($fetchedUser->firstname, $user->firstname);
         $this->assertEquals($fetchedUser->lastname, $user->lastname);
         $this->assertNotEmpty($fetchedUser->node);
+    }
+
+    public function testGetNodeByUsername()
+    {
+        $this->buildRealClient();
+        $this->clearDB();
+        $this->createUser();
+        $username = $this->standardUser->username;
+        $service = new UserService();
+        $node = $service->getNodeByUsername($username);
+        $this->assertEquals($node->getProperty('username'), $username);
+    }
+
+    private function createUser()
+    {
+        $user = $this->standardUser;
+        $userService = new UserService();
+        $userService->save($user);
     }
 }
