272dcd8dfc532fe60adfc99e8f7f01779bbc8417
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-10-31 13:50:04 +0100

    fixed conflict in Content service for CS Fixes

--- src/Service/Tag.php
+++ src/Service/Tag.php
@@ -4,8 +4,6 @@ namespace GraphStory\GraphKit\Service;
 
 use Everyman\Neo4j\Node;
 use Everyman\Neo4j\Index;
-use Everyman\Neo4j\Relationship;
-use Everyman\Neo4j\Index\NodeIndex;
 use GraphStory\GraphKit\Neo4jClient;
 
 class Tag
@@ -13,7 +11,7 @@ class Tag
     protected $node = null;
     public $id = null;
     public $tagcontent = '';
-    
+
     public static function getByNodeId($id)
     {
         $node = Neo4jClient::client()->getNode($id);
@@ -21,19 +19,19 @@ class Tag
         $content->id = $node->getId();
         $content->title = $node->getProperty('title');
         $content->url = $node->getProperty('url');
-		$content->url = $node->getProperty('tags');
+        $content->url = $node->getProperty('tags');
         $content->node = $node;
+
         return $content;
     }
 
-
-	public static function fromArray(Node $node) 
-	{
-		$tag = new Tag();
+    public static function fromArray(Node $node)
+    {
+        $tag = new Tag();
         $tag->id = $node->getId();
         $tag->tagcontent = $node->getProperty('tagcontent');
         $tag->node = $node;
-		
+
         return $user;
-	}
+    }
 }
