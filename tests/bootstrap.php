e12d351fb983ee50c40dbb0930eae14cb9f146cb
Christophe Willemsen <willemsen.christophe@gmail.com>
2014-10-31 22:24:23 +0100

    tests bs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
+if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    $loader = require $file;
+    $loader->register();
+} else {
+    throw new \RuntimeException('Composer autoloader can not be found, did you omit to run the "composer install command" ?');
+}
